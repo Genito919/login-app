3add8b579f8065f3df9b52a524edff04c616d4c7
Genito <genito919@gmail.com>
2026-03-22 20:05:32 -0500

    connect frontend to railway backend

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,5 +1,5 @@
 export const sendLogin = async (email: string, password: string) => {
-  const res = await fetch("http://localhost:3001/api/auth/login", {
+  const res = await fetch("https://login-app-production-b48f.up.railway.app/api/auth/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
